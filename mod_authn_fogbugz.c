7099998d8b3ce245d45e9b8a294f50d4ba4cf289
Jay Soffian <jaysoffian@gmail.com>
2012-05-15 18:43:12 -0400

    Remove dir merger function and cleanup code style
    Remove the dir merger function. The default is for Apache to use
    the configuration from the closest matching <Location> or
    <Directory> section which is good enough for us.
    
    Also cleanup the code style to wrap at 80 characters and clarify
    some function argument names.

--- mod_authn_fogbugz.c
+++ mod_authn_fogbugz.c
@@ -29,27 +29,22 @@
 module AP_MODULE_DECLARE_DATA authn_fogbugz_module;
 
 typedef struct {
-    const char *user;
+    const char *label;
 } authn_fogbugz_conf;
 
-/* optional function - look it up once in post_config */
-static ap_dbd_t *(*authn_fogbugz_acquire_fn)(request_rec*) = NULL;
-static void (*authn_fogbugz_prepare_fn)(server_rec*, const char*, const char*) = NULL;
+/* optional functions looked up during module init */
+static ap_dbd_t *(*authn_fogbugz_acquire_fn)(request_rec *r) = NULL;
+static void (*authn_fogbugz_prepare_fn)(server_rec *server, const char *query,
+                                        const char *label) = NULL;
 
-static void *authn_fogbugz_cr_conf(apr_pool_t *pool, char *dummy)
+static void *create_authn_fogbugz_dir_config(apr_pool_t *pool, char *dummy)
 {
     authn_fogbugz_conf *ret = apr_pcalloc(pool, sizeof(authn_fogbugz_conf));
     return ret;
 }
-static void *authn_fogbugz_merge_conf(apr_pool_t *pool, void *BASE, void *ADD)
-{
-    authn_fogbugz_conf *add = ADD;
-    authn_fogbugz_conf *base = BASE;
-    authn_fogbugz_conf *ret = apr_palloc(pool, sizeof(authn_fogbugz_conf));
-    ret->user = (add->user == NULL) ? base->user : add->user;
-    return ret;
-}
-static const char *authn_fogbugz_prepare(cmd_parms *cmd, void *cfg, const char *query)
+
+static const char *authn_fogbugz_prepare(cmd_parms *cmd, void *cfg,
+                                         const char *query)
 {
     static unsigned int label_num = 0;
     char *label;
@@ -65,16 +60,18 @@ static const char *authn_fogbugz_prepare(cmd_parms *cmd, void *cfg, const char *
 
     authn_fogbugz_prepare_fn(cmd->server, query, label);
 
-    /* save the label here for our own use */
+    /* save the label for our own use */
     return ap_set_string_slot(cmd, cfg, label);
 }
+
 static const command_rec authn_fogbugz_cmds[] =
 {
     AP_INIT_TAKE1("AuthFogBugzUserPWQuery", authn_fogbugz_prepare,
-                  (void *) APR_OFFSETOF(authn_fogbugz_conf, user), ACCESS_CONF,
+                  (void *) APR_OFFSETOF(authn_fogbugz_conf, label), ACCESS_CONF,
                   "Query used to fetch password for user"),
     {NULL}
 };
+
 static apr_status_t validate_password(request_rec* r, const char* password,
                                       const char* hash)
 {
@@ -90,8 +87,9 @@ static apr_status_t validate_password(request_rec* r, const char* password,
     char *sample = apr_pstrcat(r->pool, salt, hexdigest, NULL);
     return (strcmp(sample, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
 }
-static authn_status authn_fogbugz_password(request_rec *r, const char *user,
-                                           const char *password)
+
+static authn_status authn_fogbugz_do_auth(request_rec *r, const char *user,
+                                          const char *password)
 {
     apr_status_t rv;
     const char *dbd_password = NULL;
@@ -109,17 +107,17 @@ static authn_status authn_fogbugz_password(request_rec *r, const char *user,
         return AUTH_GENERAL_ERROR;
     }
 
-    if (conf->user == NULL) {
+    if (conf->label == NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "No AuthFogBugzUserPWQuery has been specified");
         return AUTH_GENERAL_ERROR;
     }
 
-    statement = apr_hash_get(dbd->prepared, conf->user, APR_HASH_KEY_STRING);
+    statement = apr_hash_get(dbd->prepared, conf->label, APR_HASH_KEY_STRING);
     if (statement == NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "A prepared statement could not be found for "
-                      "AuthFogBugzUserPWQuery with the key '%s'", conf->user);
+                      "AuthFogBugzUserPWQuery with the key '%s'", conf->label);
         return AUTH_GENERAL_ERROR;
     }
     if (apr_dbd_pvselect(dbd->driver, r->pool, dbd->handle, &res, statement,
@@ -150,7 +148,8 @@ static authn_status authn_fogbugz_password(request_rec *r, const char *user,
                 char *str = apr_pstrcat(r->pool, AUTHN_PREFIX,
                                         name,
                                         NULL);
-                int j = sizeof(AUTHN_PREFIX)-1; /* string length of "AUTHENTICATE_", excluding the trailing NIL */
+                /* strlen of "AUTHENTICATE_", excluding the trailing \0 */
+                int j = sizeof(AUTHN_PREFIX)-1;
                 while (str[j]) {
                     if (!apr_isalnum(str[j])) {
                         str[j] = '_';
@@ -183,22 +182,25 @@ static authn_status authn_fogbugz_password(request_rec *r, const char *user,
     return AUTH_GRANTED;
 }
 
-static void authn_fogbugz_register_hooks(apr_pool_t *p)
+static const authn_provider authn_fogbugz_provider =
 {
-    static const authn_provider authn_fogbugz_provider = {
-        &authn_fogbugz_password,
-        NULL
-    };
-    ap_register_provider(p, AUTHN_PROVIDER_GROUP, "fogbugz", "0", &authn_fogbugz_provider);
+    &authn_fogbugz_do_auth,
+    NULL
+};
+
+static void register_hooks(apr_pool_t *p)
+{
+    ap_register_provider(p, AUTHN_PROVIDER_GROUP, "fogbugz", "0",
+                         &authn_fogbugz_provider);
 }
 
 module AP_MODULE_DECLARE_DATA authn_fogbugz_module =
 {
     STANDARD20_MODULE_STUFF,
-    authn_fogbugz_cr_conf,
-    authn_fogbugz_merge_conf,
-    NULL,
-    NULL,
-    authn_fogbugz_cmds,
-    authn_fogbugz_register_hooks
+    create_authn_fogbugz_dir_config, /* dir config creater */
+    NULL,                            /* dir merger --- default is to override */
+    NULL,                            /* server config */
+    NULL,                            /* merge server config */
+    authn_fogbugz_cmds,              /* command apr_table_t */
+    register_hooks                   /* register hooks */
 };
