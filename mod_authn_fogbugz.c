22dd32936b99f98b1bb31be9bcad398ff6a2d21c
Jay Soffian <jaysoffian@gmail.com>
2012-05-15 21:41:50 -0400

    Add support for sPasswordVersion 840
    Add support for sPasswordVersion 840 introduced by recent versions of
    FogBugz. Since we now need the password version to know which hashing
    routine to run, drop support for the AuthFogBugzUserPWQuery argument.
    
    Instead, assume the correct SQL statement will be prepared via the DBD
    module's DBDPrepareSQL directive.

--- mod_authn_fogbugz.c
+++ mod_authn_fogbugz.c
@@ -27,104 +27,137 @@
 #include <util_md5.h>
 
 module AP_MODULE_DECLARE_DATA authn_fogbugz_module;
+static APR_OPTIONAL_FN_TYPE(ap_dbd_acquire) *authn_fogbugz_acquire;
 
 typedef struct {
     const char *label;
 } authn_fogbugz_conf;
 
-/* optional functions looked up during module init */
-static ap_dbd_t *(*authn_fogbugz_acquire_fn)(request_rec *r) = NULL;
-static void (*authn_fogbugz_prepare_fn)(server_rec *server, const char *query,
-                                        const char *label) = NULL;
-
-static void *create_authn_fogbugz_dir_config(apr_pool_t *pool, char *dummy)
+static void *create_authn_fogbugz_dir_config(apr_pool_t *p, char *dummy)
 {
-    authn_fogbugz_conf *ret = apr_pcalloc(pool, sizeof(authn_fogbugz_conf));
-    return ret;
+    authn_fogbugz_conf *conf = apr_palloc(p, sizeof(*conf));
+    conf->label = "fogbugz";
+    return conf;
 }
 
-static const char *authn_fogbugz_prepare(cmd_parms *cmd, void *cfg,
-                                         const char *query)
+static const char *set_authn_label_slot(cmd_parms *cmd, void *offset,
+                                        const char *label)
 {
-    static unsigned int label_num = 0;
-    char *label;
-
-    if (authn_fogbugz_prepare_fn == NULL) {
-        authn_fogbugz_prepare_fn = APR_RETRIEVE_OPTIONAL_FN(ap_dbd_prepare);
-        if (authn_fogbugz_prepare_fn == NULL) {
-            return "You must load mod_dbd to enable AuthFogBugz functions";
-        }
-        authn_fogbugz_acquire_fn = APR_RETRIEVE_OPTIONAL_FN(ap_dbd_acquire);
-    }
-    label = apr_psprintf(cmd->pool, "authn_fogbugz_%d", ++label_num);
-
-    authn_fogbugz_prepare_fn(cmd->server, query, label);
-
-    /* save the label for our own use */
-    return ap_set_string_slot(cmd, cfg, label);
-}
+    return ap_set_string_slot(cmd, offset, label);
+};
 
 static const command_rec authn_fogbugz_cmds[] =
 {
-    AP_INIT_TAKE1("AuthFogBugzUserPWQuery", authn_fogbugz_prepare,
-                  (void *) APR_OFFSETOF(authn_fogbugz_conf, label), ACCESS_CONF,
-                  "Query used to fetch password for user"),
+    AP_INIT_TAKE1("AuthFogBugzQueryLabel", set_authn_label_slot,
+                  (void *)APR_OFFSETOF(authn_fogbugz_conf, label),
+                  ACCESS_CONF,
+                  "Label of the SQL statement prepared via the DBDPrepareSQL "
+                  "directive (defaults to 'fogbugz') used to query "
+                  "authentication details."),
     {NULL}
 };
 
-static apr_status_t validate_password(request_rec* r, const char* password,
-                                      const char* hash)
+static const char *encrypt_password_600(request_rec* r,
+                                        const char* cleartext,
+                                        const char* salt)
 {
-    int hash_len = strlen(hash);
-    if (hash_len != 40) {
-        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                      "Invalid hashed password length, %d", hash_len);
-        return AUTH_GENERAL_ERROR;
-    }
-    char *salt = apr_pstrndup(r->pool, hash, 8);
-    char *salted_pw = apr_pstrcat(r->pool, password, salt, NULL);
-    char *hexdigest = ap_md5(r->pool, (unsigned char *) salted_pw);
-    char *sample = apr_pstrcat(r->pool, salt, hexdigest, NULL);
-    return (strcmp(sample, hash) == 0) ? APR_SUCCESS : APR_EMISMATCH;
+    const char *salted = apr_pstrcat(r->pool, cleartext, salt, NULL);
+    const char *hashed = ap_md5(r->pool, (unsigned char *) salted);
+    return apr_pstrcat(r->pool, salt, hashed, NULL);
 }
 
-static authn_status authn_fogbugz_do_auth(request_rec *r, const char *user,
-                                          const char *password)
+static const char *encrypt_password_840(request_rec* r,
+                                        const char* cleartext,
+                                        const char* salt)
+{
+    int outer = 100, inner;
+    char hash[4000]; /* large enough for 100 iterations of md5 hexdigest */
+    char *cp;
+    apr_pool_t *sp;
+    apr_pool_create(&sp, r->pool);
+    /* sHash = MD5Hash(sPassword & sSalt) */
+    strcpy(hash, ap_md5(sp, (unsigned char *)
+        apr_pstrcat(sp, cleartext, salt, NULL)));
+    cp = hash + 32;
+    while (outer-- > 0) {
+        apr_pool_clear(sp);
+        inner = outer;
+        while (inner-- > 0) {
+            /* sHash = sHash & MD5Hash(sHash & sPassword & sSalt) */
+            strcpy(cp, ap_md5(sp, (unsigned char *)
+                apr_pstrcat(sp, hash, cleartext, salt, NULL)));
+            cp += 32;
+            apr_pool_clear(sp);
+        }
+        /* sHash = MD5Hash(sHash) */
+        strcpy(hash, ap_md5(sp, (unsigned char *) hash));
+        cp = hash + 32;
+    }
+    apr_pool_destroy(sp);
+    /* sHash = sSalt & MD5Hash(sHash) */
+    return apr_pstrcat(r->pool,
+        salt, ap_md5(r->pool, (unsigned char *) hash), NULL);
+}
+
+static apr_status_t compare_passwords(request_rec* r,
+                                      const char* cleartext,
+                                      const char* encrypted,
+                                      const char* pwversion)
+{
+    const char *sample = NULL;
+    const char *salt = NULL;
+    int encrypted_len = strlen(encrypted);
+    if (encrypted_len != 40) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                      "Invalid encrypted password length, %d", encrypted_len);
+        return AUTH_GENERAL_ERROR;
+    }
+    salt = apr_pstrndup(r->pool, encrypted, 8);
+    if (strcmp(pwversion, "600") == 0) {
+        sample = encrypt_password_600(r, cleartext, salt);
+    } else if (strcmp(pwversion, "840") == 0) {
+        sample = encrypt_password_840(r, cleartext, salt);
+    } else {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                      "Unsupported sPasswordVersion, %s", pwversion);
+        return AUTH_GENERAL_ERROR;
+    }
+    return (strcmp(sample, encrypted) == 0) ? APR_SUCCESS : APR_EMISMATCH;
+}
+
+static authn_status authn_fogbugz_check_password(request_rec *r,
+                                                 const char *username,
+                                                 const char *password)
 {
     apr_status_t rv;
-    const char *dbd_password = NULL;
+    const char *encrypted = NULL;
+    const char *pwversion = NULL;
     apr_dbd_prepared_t *statement;
     apr_dbd_results_t *res = NULL;
     apr_dbd_row_t *row = NULL;
 
     authn_fogbugz_conf *conf = ap_get_module_config(r->per_dir_config,
                                                     &authn_fogbugz_module);
-    ap_dbd_t *dbd = authn_fogbugz_acquire_fn(r);
+    ap_dbd_t *dbd = authn_fogbugz_acquire(r);
     if (dbd == NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "Failed to acquire database connection to look up "
-                      "user '%s'", user);
-        return AUTH_GENERAL_ERROR;
-    }
-
-    if (conf->label == NULL) {
-        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                      "No AuthFogBugzUserPWQuery has been specified");
+                      "user '%s'", username);
         return AUTH_GENERAL_ERROR;
     }
 
     statement = apr_hash_get(dbd->prepared, conf->label, APR_HASH_KEY_STRING);
     if (statement == NULL) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                      "A prepared statement could not be found for "
-                      "AuthFogBugzUserPWQuery with the key '%s'", conf->label);
+                      "A prepared statement with the key '%s' could not be "
+                      "found", conf->label);
         return AUTH_GENERAL_ERROR;
     }
     if (apr_dbd_pvselect(dbd->driver, r->pool, dbd->handle, &res, statement,
-                              0, user, NULL) != 0) {
+                              0, username, NULL) != 0) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "Query execution error looking up '%s' "
-                      "in database", user);
+                      "in database", username);
         return AUTH_GENERAL_ERROR;
     }
     for (rv = apr_dbd_get_row(dbd->driver, r->pool, res, &row, -1);
@@ -133,48 +166,22 @@ static authn_status authn_fogbugz_do_auth(request_rec *r, const char *user,
         if (rv != 0) {
             ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r,
                           "Error retrieving results while looking up '%s' "
-                          "in database", user);
+                          "in database", username);
             return AUTH_GENERAL_ERROR;
         }
-        if (dbd_password == NULL) {
-#if APU_MAJOR_VERSION > 1 || (APU_MAJOR_VERSION == 1 && APU_MINOR_VERSION >= 3)
-            /* add the rest of the columns to the environment */
-            int i = 1;
-            const char *name;
-            for (name = apr_dbd_get_name(dbd->driver, res, i);
-                 name != NULL;
-                 name = apr_dbd_get_name(dbd->driver, res, i)) {
+        if (encrypted == NULL) {
+            encrypted = apr_dbd_get_entry(dbd->driver, row, 0);
+            pwversion = apr_dbd_get_entry(dbd->driver, row, 1);
 
-                char *str = apr_pstrcat(r->pool, AUTHN_PREFIX,
-                                        name,
-                                        NULL);
-                /* strlen of "AUTHENTICATE_", excluding the trailing \0 */
-                int j = sizeof(AUTHN_PREFIX)-1;
-                while (str[j]) {
-                    if (!apr_isalnum(str[j])) {
-                        str[j] = '_';
-                    }
-                    else {
-                        str[j] = apr_toupper(str[j]);
-                    }
-                    j++;
-                }
-                apr_table_set(r->subprocess_env, str,
-                              apr_dbd_get_entry(dbd->driver, row, i));
-                i++;
-            }
-#endif
-            dbd_password = apr_dbd_get_entry(dbd->driver, row, 0);
         }
         /* we can't break out here or row won't get cleaned up */
     }
 
-    if (!dbd_password) {
+    if (!encrypted) {
         return AUTH_USER_NOT_FOUND;
     }
 
-    rv = validate_password(r, password, dbd_password);
-
+    rv = compare_passwords(r, password, encrypted, pwversion);
     if (rv != APR_SUCCESS) {
         return AUTH_DENIED;
     }
@@ -184,14 +191,19 @@ static authn_status authn_fogbugz_do_auth(request_rec *r, const char *user,
 
 static const authn_provider authn_fogbugz_provider =
 {
-    &authn_fogbugz_do_auth,
-    NULL
+    &authn_fogbugz_check_password,
 };
+
+static void ImportDBDOptFn(void)
+{
+    authn_fogbugz_acquire = APR_RETRIEVE_OPTIONAL_FN(ap_dbd_acquire);
+}
 
 static void register_hooks(apr_pool_t *p)
 {
     ap_register_provider(p, AUTHN_PROVIDER_GROUP, "fogbugz", "0",
                          &authn_fogbugz_provider);
+    ap_hook_optional_fn_retrieve(ImportDBDOptFn, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
 module AP_MODULE_DECLARE_DATA authn_fogbugz_module =
